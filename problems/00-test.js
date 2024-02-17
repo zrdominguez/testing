f3906a2f0bf502d6f7df7f1ceb4f234dd246dbfe
zrdominguez <zechariahd@gmail.com>
2024-02-17 01:47:27 -0600

    commit test

---
+++ problems/00-test.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function, `helloWorld`, that will return the string of 'Hello World!'
+
+*/
+
+
+function helloWorld() {
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = helloWorld;
+} catch (e) {
+  module.exports = null;
+}
